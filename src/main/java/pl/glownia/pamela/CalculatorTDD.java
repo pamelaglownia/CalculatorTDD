1e29fe630256253d20720b45be9289dced614c78
Pam <pamela.glownia@gmail.com>
2020-05-28 07:19:10 +0200

    Add test for adding two numbers and fail

--- src/main/java/pl/glownia/pamela/CalculatorTDD.java
+++ src/main/java/pl/glownia/pamela/CalculatorTDD.java
@@ -1,12 +1,12 @@
 package pl.glownia.pamela;
+
 import java.util.Scanner;
 
 class CalculatorTDD {
 
-  public static void main (String[] args) {
+    public static void main(String[] args) {
 
-      Scanner takeData = new Scanner(System.in);
-      Calculator calculator = new Calculator(takeData);
-      calculator.calculate();
- }
+        Scanner takeData = new Scanner(System.in);
+        Calculator calculator = new Calculator(takeData);
+    }
 }
--- src/test/pl/glownia/pamela/CalculatorTest.java
+++ src/test/pl/glownia/pamela/CalculatorTest.java
@@ -1,4 +1,28 @@
 package pl.glownia.pamela;
 
-class CalculatorTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Scanner;
+
+public class CalculatorTest {
+    private Calculator calculator;
+    private Scanner takeData;
+
+    @Before
+    public void setUp() throws Exception {
+        this.calculator = new Calculator(takeData);
+    }
+
+    @Test
+    public void ShouldAddTwoNumbers() {
+        // Given
+        double firstNumber = 1.0;
+        double secondNumber = 2.0;
+        // When
+        double result = this.calculator.add(firstNumber, secondNumber);
+        // Then
+        Assert.assertEquals("3.0", result);
+    }
 }
